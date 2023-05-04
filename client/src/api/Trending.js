603cb9c882ff5c4458e1162cf927ea75ee08f634
abduljabar <abduljabar.nur.8@gmail.com>
2023-05-04 13:46:41 -0500

    deploying

--- client/src/api/Trending.js
+++ client/src/api/Trending.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const search = async (query) =>
-  axios.get(`https://api.themoviedb.org/3/trending/tv/week?api_key=${process.env.REACT_APP_TMDB_API_KEY}`);
+  axios.get(`https://api.themoviedb.org/3/trending/tv/week?api_key=b508e614477c137da7c8dc95579b02ac`);
   const trendingMovies = async (query) =>
-  axios.get(`https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.REACT_APP_TMDB_API_KEY}`);
+  axios.get(`https://api.themoviedb.org/3/trending/movie/week?api_key=b508e614477c137da7c8dc95579b02ac`);
   const inTheaters = async (query) =>
   axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1&region=us`);
 
